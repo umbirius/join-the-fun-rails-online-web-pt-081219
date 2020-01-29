5b01e8f735dc6dc36f820e7ec0c9c7f8dfee1655
umberto palazzo <upalazzo93@gmail.com>
2020-01-28 21:29:15 -0500

    Done.

---
+++ db/migrate/20200129022546_add_taxi_idand_passenger_id_to_rides.rb
@@ -0,0 +1,8 @@
+class AddTaxiIdandPassengerIdToRides < ActiveRecord::Migration[5.0]
+  def change
+    change_table :rides do |t|
+      t.integer :taxi_id
+      t.integer :passenger_id
+    end
+  end
+end
